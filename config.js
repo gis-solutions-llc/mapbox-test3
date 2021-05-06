7983ec7d45b90033464b432f7749d25a3f0e30e8
gis-solutions-llc <38801237+gis-solutions-llc@users.noreply.github.com>
2021-05-06 10:18:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   style: "mapbox://styles/mapbox/light-v10",
   accessToken:
-    "pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA",
+    "pk.eyJ1IjoiZ2lzc29sdXRpb25zbGxjbWFwcyIsImEiOiJjamw4Y2RiMDAweGx4M2txb3NxZnBxeDFoIn0.MfwIqWM5qlGDkyDOpXmQFQ",
   CSV: "./Sample_Data.csv",
   center: [-120.234, 47.398],
   zoom: 6,
